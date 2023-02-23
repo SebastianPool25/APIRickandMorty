eac74b890049d147ffe4d998158f5a6277d096de
SebastianPool25 <jose.19070054@itsmotul.edu.mx>
2023-02-23 16:33:16 -0600

    Avanzando sobre el about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,13 @@
 <template>
-  <body class="bg-green-500">
-  <div class="about">
-    <h1>This is an about page</h1>
+  <body class="bg-green-500 p-5 grid grid-cols-2 gap-2">
+  <div class="font-mono">
+    <h1 class="text-4xl font-bold p-9 m-11">Mi nombre es José Sebastián Pool Cervates</h1>
+  </div>
+
+  <div class="p-5 m-11">
+    <p class="text-lg p-9">Está página esta hecha con el fin de poder aplicar la API de Rick and Morty, para entender como es que funciona
+      tanto AXIOS para las API y como se puede implementar en VUEJS 3</p>
+    <img src="../assets/pickle.gif" alt="no hay!" class="w-80 ml-11">
   </div>
   </body>
 </template>
