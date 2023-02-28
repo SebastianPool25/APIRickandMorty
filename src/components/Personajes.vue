9899e7300a447496d2091bae8a2b35953baabcb0
SebastianPool25 <jose.19070054@itsmotul.edu.mx>
2023-02-28 07:58:14 -0600

    botones funcionando

--- src/components/Personajes.vue
+++ src/components/Personajes.vue
@@ -9,7 +9,7 @@ export default {
     return {
       info: [],
       personajes: [],
-      cont:2
+      cont:1,
     }
   },
 
@@ -23,7 +23,7 @@ export default {
 
   methods: {
     pag(num) {
-      API_URL='https://rickandmortyapi.com/api/character/?page='+num
+      API_URL='https://rickandmortyapi.com/api/character/?page='+this.cont
       console.log(API_URL)
       axios.get(API_URL)
       .then((response) => {
@@ -31,7 +31,6 @@ export default {
         this.info = response.data.info;
         this.personajes = response.data.results;
       })
-      this.cont++
     }
   },
 
@@ -47,7 +46,7 @@ export default {
       <button @click="" class="my-2">{{ p.name }}</button>
     </li>
   </ul>
-  <button @click="pag (cont - 1)" class="my-5 bg-green-200 rounded-lg text-lg mx-5">Regresar</button>
+  <button  v-if="cont > 1" @click="pag(cont--)" class="my-5 bg-green-200 rounded-lg text-lg mx-5">Regresar</button>
   <button>{{ count }}</button>
-  <button @click="pag(cont)" class="my-5 bg-green-200 rounded-lg text-lg mx-5">Siguiente</button>
+  <button v-if="cont <= 42" @click="pag(cont++)" class="my-5 bg-green-200 rounded-lg text-lg mx-5">Siguiente</button>
 </template>
